9fc185bfca5e46802bc6cc1ff3ae2dd686b6de04
coytk <coy19950407@gmail.com>
2021-03-20 00:34:18 +0900

    init

--- src/App.java
+++ src/App.java
@@ -1,20 +1,50 @@
-public class App {
-       
 
-    public static void main(String[] args) throws Exception {
+class Dog{
+    int size;
+    String breed;
+    String name;
 
-        boolean flag=true;
-        int x=20;
-        while(flag){
+    void bark(){
+        System.out.println("Ruff!");
+    }
+}
+class Alarm{
+    int alarmtimes;
 
-            System.out.printf("x = %d\n",x);
-            if(x==1){
-                flag=false;
+    void alarm(){
+        System.out.println("Wake up!!!");
+    }
+
+}
+class Dogtest {
+    public static void main(String[] args) {
+        Dog dog=new Dog();
+        Alarm alarm=new Alarm();
+        dog.size=40;
+        dog.name="Coy";
+        int almtime=0;
+        //System.out.println("Dog say: "+ dog.size);
+        for (int timing = 1; timing <= 5; timing++) {
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException e){
             }
-            x=x-1;
-            
+            System.out.printf("%d sec \n",timing);
+            if (timing==5) {
+                alarm.alarm();
+                almtime++;
+                if (almtime==5) {
+                    dog.bark();
+                    break;
+                }
+                timing=0;
+            }
         }
-        System.out.println("End Test");
+
+
+
+        //dog.bark();
+
     }
 
 
